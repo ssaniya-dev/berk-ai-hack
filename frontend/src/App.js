4a797a89580dd766938f975c5c3f0681596a399d
Saniya Savla <ssaniyasavla@gmail.com>
2024-06-22 17:51:23 -0700

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import Main from './components/Main';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
-      <Main/>
+    <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/" element={<Login />} />
+    </Routes>
+  </Router>
   );
 }
 
