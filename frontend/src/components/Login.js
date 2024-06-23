134ca5c58f0be4b21b2f222f87760ad6b4ec5cc5
Saniya Savla <ssaniyasavla@gmail.com>
2024-06-22 19:00:44 -0700

    signing up

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -26,7 +26,7 @@ const Login = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-4xl font-bold mb-4 bg-gradient-to-r from-blue-500 via-purple-500 to-blue-400 text-transparent bg-clip-text animate-gradient">Login</h2>
+        <h2 className="text-4xl font-bold mb-4 bg-gradient-to-r from-blue-500 via-purple-500 to-blue-400 text-transparent bg-clip-text animate-gradient">Login / Sign Up</h2>
         <form onSubmit={handleLogin}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -63,11 +63,11 @@ const Login = () => {
             </button>
           </div>
         </form>
-        <p className="text-center text-gray-600 mt-4">
-          Don't have an account?{' '}
-          <Link to="/register" className="text-blue-500 hover:text-blue-700">
+        <p className="text-center text-gray-600 mt-1">
+          Don't have an account? Just sign up with your new credentials!{' '}
+          {/* <Link to="/register" className="text-blue-500 hover:text-blue-700">
             Register
-          </Link>
+          </Link> */}
         </p>
       </div>
     </div>
